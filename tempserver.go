02cc92d4ad797ce346a66c02681bbb3630846c61
Lenovo <per.e.olofsson@gmail.com>
2022-04-27 21:13:46 +0200

    Cleanup

--- tempserver.go
+++ tempserver.go
@@ -1,62 +1,61 @@
 package main
 
 import (
-	"net/http"
 	"database/sql"
-	"fmt"
-	"log"
-	"time"
-	"flag"
-	"github.com/magiconair/properties"
-	_ "github.com/mattn/go-sqlite3"
-    "github.com/gorilla/mux"	
 	"encoding/json"
+	"flag"
+	"fmt"
 	"github.com/go-echarts/go-echarts/v2/charts"
 	"github.com/go-echarts/go-echarts/v2/opts"
 	"github.com/go-echarts/go-echarts/v2/types"
+	"github.com/gorilla/mux"
+	"github.com/magiconair/properties"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"net/http"
+	"time"
 )
 
 var db *sql.DB
 var cfg Config
 
 type Config struct {
-	DbFile   string `properties:"dbfile"`
+	DbFile string `properties:"dbfile"`
 }
 
 func generateLineItems(name int, count int) []opts.LineData { //nolint:unparam
 	items := make([]opts.LineData, 0)
 
-//	items := make([]Reading, 0)
+	//	items := make([]Reading, 0)
 
 	rows, err := db.Query("SELECT Id FROM reading ORDER BY ID DESC LIMIT 1")
 	Check(err)
 	defer rows.Close()
 	lastid := 0
-    for rows.Next() {
+	for rows.Next() {
 		read := Reading{}
-		if err = rows.Scan( &read.Id); err != nil {
+		if err = rows.Scan(&read.Id); err != nil {
 			log.Fatalf("could not scan row: %v", err)
 		}
 		lastid = read.Id
-	}	
+	}
 	rows, err = db.Query("SELECT Sensor, Temperature FROM reading  WHERE ID >= $1  AND Sensor = $2 LIMIT $3", lastid-count, name, count)
 
-//	rows, err := db.Query("SELECT Sensor, Temperature FROM reading ORDER BY ID DESC LIMIT $1 WHERE Sensor = $2", count, name)
+	//	rows, err := db.Query("SELECT Sensor, Temperature FROM reading ORDER BY ID DESC LIMIT $1 WHERE Sensor = $2", count, name)
 	Check(err)
 	defer rows.Close()
-    for rows.Next() {
+	for rows.Next() {
 		read := Reading{}
 		// create an instance of `Bird` and write the result of the current row into it
-		if err = rows.Scan( &read.Sensor, &read.Temperature); err != nil {
+		if err = rows.Scan(&read.Sensor, &read.Temperature); err != nil {
 			log.Fatalf("could not scan row: %v", err)
 		}
-		//fmt.Printf("found read: %+v\n", read)	
-			// append the current instance to the slice of birds
-		items = append(items, opts.LineData{Value:read.Temperature})
+		//fmt.Printf("found read: %+v\n", read)
+		// append the current instance to the slice of birds
+		items = append(items, opts.LineData{Value: read.Temperature})
 	}
 	return items
 }
-
 
 func httpserver(w http.ResponseWriter, _ *http.Request) {
 	// create a new line instance
@@ -71,11 +70,11 @@ func httpserver(w http.ResponseWriter, _ *http.Request) {
 
 	// Put data into instance
 	line.SetXAxis([]string{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"}).
-		AddSeries("28-1b61221e64ff", generateLineItems(1,100)).
-		AddSeries("28-7167221e64ff", generateLineItems(2,100)).
-		AddSeries("28-7167221e64ff", generateLineItems(3,100)).
-		AddSeries("28-7167221e64ff", generateLineItems(4,100)).
-		AddSeries("28-7167221e64ff", generateLineItems(5,100)).
+		AddSeries("28-1b61221e64ff", generateLineItems(1, 100)).
+		AddSeries("28-7167221e64ff", generateLineItems(2, 100)).
+		AddSeries("28-7167221e64ff", generateLineItems(3, 100)).
+		AddSeries("28-7167221e64ff", generateLineItems(4, 100)).
+		AddSeries("28-7167221e64ff", generateLineItems(5, 100)).
 		SetSeriesOptions(charts.WithLineChartOpts(opts.LineChart{Smooth: true}))
 	err := line.Render(w)
 	Check(err)
@@ -83,54 +82,54 @@ func httpserver(w http.ResponseWriter, _ *http.Request) {
 
 func initdb() {
 	var err error
-    db, err = sql.Open("sqlite3", cfg.DbFile) 
-    Check(err)
+	db, err = sql.Open("sqlite3", cfg.DbFile)
+	Check(err)
 }
 
 type Reading struct {
-	Id  		int 		`json:"id"`
-	Sensor 		int 		`json:"sensor"`
-    TimeStamp   time.Time 	`json:"timeStamp"`
-    Temperature float64		`json:"temperature"`
+	Id          int       `json:"id"`
+	Sensor      int       `json:"sensor"`
+	TimeStamp   time.Time `json:"timeStamp"`
+	Temperature float64   `json:"temperature"`
 }
 
 func Check(e error) {
-    if e != nil {
-            panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
-func returnLast(w http.ResponseWriter, r *http.Request){
-    fmt.Println("Endpoint Hit: returnLast")
+func returnLast(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnLast")
 
 	items := make([]Reading, 0)
 
 	rows, err := db.Query("SELECT Id, Sensor, Temperature, Datetime FROM reading ORDER BY ID DESC LIMIT $1", 50)
 	Check(err)
 	defer rows.Close()
-    for rows.Next() {
+	for rows.Next() {
 		read := Reading{}
 		timestring := ""
 		// create an instance of `Bird` and write the result of the current row into it
-		if err := rows.Scan(&read.Id, &read.Sensor, &read.Temperature, &timestring); err != nil {
+		if err = rows.Scan(&read.Id, &read.Sensor, &read.Temperature, &timestring); err != nil {
 			log.Fatalf("could not scan row: %v", err)
 		}
 		layout := "2006-01-02T15:04:05Z"
-		temp, err := time.Parse(layout, timestring)
-		Check(err)
+		temp, err2 := time.Parse(layout, timestring)
+		Check(err2)
 		read.TimeStamp = temp
 
-		fmt.Printf("found read: %+v + %s\n", read, timestring)	
-			// append the current instance to the slice of birds
+		fmt.Printf("found read: %+v + %s\n", read, timestring)
+		// append the current instance to the slice of birds
 		items = append(items, read)
 	}
 
-    err = json.NewEncoder(w).Encode(items)
+	err = json.NewEncoder(w).Encode(items)
 	Check(err)
 }
 
-func getStart(w http.ResponseWriter, r *http.Request){
-    fmt.Println("Endpoint Hit: getStart")
+func getStart(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: getStart")
 
 	eventID := mux.Vars(r)["id"]
 
@@ -139,24 +138,24 @@ func getStart(w http.ResponseWriter, r *http.Request){
 	rows, err := db.Query("SELECT Id, Sensor, Temperature, Datetime FROM reading  WHERE ID >= ?  LIMIT $1", eventID, 50)
 	Check(err)
 	defer rows.Close()
-    for rows.Next() {
+	for rows.Next() {
 		read := Reading{}
 		timestring := ""
 		// create an instance of `Bird` and write the result of the current row into it
-		if err := rows.Scan(&read.Id, &read.Sensor, &read.Temperature, &timestring); err != nil {
+		if err = rows.Scan(&read.Id, &read.Sensor, &read.Temperature, &timestring); err != nil {
 			log.Fatalf("could not scan row: %v", err)
 		}
 		layout := "2006-01-02T15:04:05Z"
-		temp, err := time.Parse(layout, timestring)
-		Check(err)
-		read.TimeStamp = temp.Add(time.Hour*2)
+		temp, err2 := time.Parse(layout, timestring)
+		Check(err2)
+		read.TimeStamp = temp.Add(time.Hour * 2)
 
-		fmt.Printf("found read: %+v + %s\n", read, timestring)	
-			// append the current instance to the slice of birds
+		fmt.Printf("found read: %+v + %s\n", read, timestring)
+		// append the current instance to the slice of birds
 		items = append(items, read)
 	}
 
-    err = json.NewEncoder(w).Encode(items)
+	err = json.NewEncoder(w).Encode(items)
 	Check(err)
 }
 
@@ -170,19 +169,18 @@ func main() {
 		log.Fatal(err)
 	}
 
-
 	initdb()
 
 	if err := db.Ping(); err != nil {
 		log.Fatalf("unable to reach database: %v", err)
 	}
-	fmt.Println("database is reachable")	
+	fmt.Println("database is reachable")
 
 	myRouter := mux.NewRouter().StrictSlash(true)
 
-	myRouter.HandleFunc("/", httpserver)	
+	myRouter.HandleFunc("/", httpserver)
 	myRouter.HandleFunc("/last", returnLast)
 	myRouter.HandleFunc("/start/{id}", getStart).Methods("GET")
 
-    log.Fatal(http.ListenAndServe(":8081", myRouter))
+	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
